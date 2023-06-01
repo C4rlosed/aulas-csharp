da414def3c48619ab00661e2a962371f65258045
Carlos Eduardo Pereira Bento <107080282+C4rlosed@users.noreply.github.com>
2023-05-31 21:44:41 -0300

    Update aula-matriz-ex.cs

--- aula-matriz-ex.cs
+++ aula-matriz-ex.cs
@@ -1,8 +1,9 @@
 Random rnd = new Random();
 
 int[][] Matriz;
-int NCol = rnd.Next(0,100);
+int NCol = rnd.Next(0, 100);
 int NLinhas = rnd.Next(0, 100);
+
 
 Matriz = new int[NLinhas][];
 for (int lin = 0; lin < NLinhas; lin++)
@@ -18,7 +19,8 @@ for (int lin = 0; lin < NLinhas; lin++)
 Console.Write("Digite o número que deseja conferir se está na matriz: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int contador = 0;
-int Maior = Matriz[0][0];
+
+
 for (int lin = 0; lin < NLinhas; lin++)
 {
     for (int col = 0; col < NCol; col++)
@@ -28,10 +30,27 @@ for (int lin = 0; lin < NLinhas; lin++)
         {
             contador++;
         }
-
     }
     Console.WriteLine();
 }
+
+//Verificar qual o maior número da matriz
+int Maior = Matriz[0][0];
+//Enquanto linhas for menor que N de Linhas, lin++
+for(int lin = 0; lin < NLinhas; lin++)
+{
+    //Enquanto colunas for menor que N de Colunas, col++
+    for (int col = 0;col < NCol; col++)
+    {
+        //Se Maior for menor que os números de linhas e os números de colunas
+        if(Maior < Matriz[lin][col])
+        {
+            //Maior é igual a quantidade de lin e col na Matriz
+            Maior = Matriz[lin][col];
+        }
+    }
+}
+
 
 Console.WriteLine("MAIOR NÚMERO DA MATRIZ:" + Maior);
 Console.WriteLine("O número " + num + " aparece " + contador + " vezes");
